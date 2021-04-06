95f44ec2b30b293328dcf810467db2eb7fe53363
Sdsdsde2 <imspammails31@gmail.com>
2021-04-06 17:40:32 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("create-task-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const newDesc = document.getElementById("new-task-description").value;
+    const newTask = document.createElement("li");
+    newTask.innerText = newDesc;
+    appendNewTask(newTask);
+    //document.getElementById("list").appendChild(newDesc)
+  });
 });
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
